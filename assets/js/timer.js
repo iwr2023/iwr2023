d7fb5de001d2ba592faafe904da5e17a2f881b7a
donmahallem <donmahallem@users.noreply.github.com>
2022-10-03 20:33:39 +0200

    chore: save

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,18 +1,66 @@
-class LongName extends HTMLElement {
+class AppCountdown extends HTMLElement {
+
+  // Can define constructor arguments if you wish.
   constructor() {
-    // Always call super first in constructor
+    // If you define a constructor, always call super() first!
+    // This is specific to CE and required by the spec.
     super();
-    console.log("setup2");
+
     this._shadowRoot = this.attachShadow({ mode: "open" });
-    const wrapper = document.createElement("span");
-    wrapper.innerHTML = "asdddf";
-    shadow.appendChild(wrapper);
-    // Element functionality written in here
+    this.timerDisplay = document.createElement("span");
+    this._shadowRoot.appendChild(this.timerDisplay);
+    this.timerId = undefined;
+    this.targetDate = new Date().getTime()+2003030;
   }
   connectedCallback() {
-    console.info("connected");
-    this.innerHTML = "Hello"; //can't be set in constructor()
+    this.updateTargetDate(this.getAttribute('target-date'));
+    this.startTimer();
+  }
+  startTimer(){
+    this.timerId = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = this.targetDate - now;
+      if (distance < 0) {
+        clearInterval(this.timerId);
+        this.timerId=undefined;
+        this.timerDisplay.innerHTML = "ARRIVED";
+      }else{
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        this.timerDisplay.innerHTML = days + "d " + hours + "h "
+          + minutes + "m " + seconds + "s ";
+      }
+    }, 1000);
+  }
+  static get observedAttributes() { console.log("obser");return ['target-date']; }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    if(name=="target-date"){
+      this.updateTargetDate(newValue);
+    }
+  }
+
+  updateTargetDate(inp){
+    try{
+      this.targetDate=new Date(inp).getTime();
+      if(!this.timerId){
+        this.startTimer();
+      }
+    }catch(e){
+      console.error(`Could not parse Date "${inp}"`);
+      this.targetDate=0;
+    }
+  }
+  
+  disconnectedCallback() {
+    console.log("disconnected");
+    clearInterval(this.timerId);
+  }
+  toggleDrawer() {
   }
 }
-console.log("setup");
-customElements.define("iwr-asdf", LongName, { extends: "div" });
+
+customElements.define('app-countdown', AppCountdown);
