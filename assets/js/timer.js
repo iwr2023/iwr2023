7527dfbb16a2a85b903e95437bfe439d01aaa50f
donmahallem <donmahallem@users.noreply.github.com>
2022-10-29 17:43:42 +0200

    feat(timer): better timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,66 +1,70 @@
 class AppCountdown extends HTMLElement {
-
   // Can define constructor arguments if you wish.
   constructor() {
     // If you define a constructor, always call super() first!
     // This is specific to CE and required by the spec.
     super();
 
-    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._shadowRoot = this.attachShadow({ mode: "closed" });
     this.timerDisplay = document.createElement("span");
+    const linkElem = document.createElement("style");
+    linkElem.innerHTML = "{{ .Content }}";
+    this._shadowRoot.appendChild(linkElem);
     this._shadowRoot.appendChild(this.timerDisplay);
     this.timerId = undefined;
-    this.targetDate = new Date().getTime()+2003030;
+    this.targetDate = new Date().getTime() + 2003030;
   }
   connectedCallback() {
-    this.updateTargetDate(this.getAttribute('target-date'));
+    this.updateTargetDate(this.getAttribute("target-date"));
     this.startTimer();
   }
-  startTimer(){
+  startTimer() {
     this.timerId = setInterval(() => {
       const now = new Date().getTime();
 
       const distance = this.targetDate - now;
       if (distance < 0) {
         clearInterval(this.timerId);
-        this.timerId=undefined;
+        this.timerId = undefined;
         this.timerDisplay.innerHTML = "ARRIVED";
-      }else{
+      } else {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        this.timerDisplay.innerHTML = days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ";
+        this.timerDisplay.innerHTML =
+          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       }
     }, 1000);
   }
-  static get observedAttributes() { console.log("obser");return ['target-date']; }
-  
+  static get observedAttributes() {
+    return ["target-date"];
+  }
+
   attributeChangedCallback(name, oldValue, newValue) {
-    if(name=="target-date"){
+    if (name == "target-date") {
       this.updateTargetDate(newValue);
     }
   }
 
-  updateTargetDate(inp){
-    try{
-      this.targetDate=new Date(inp).getTime();
-      if(!this.timerId){
+  updateTargetDate(inp) {
+    try {
+      this.targetDate = new Date(inp).getTime();
+      if (!this.timerId) {
         this.startTimer();
       }
-    }catch(e){
+    } catch (e) {
       console.error(`Could not parse Date "${inp}"`);
-      this.targetDate=0;
+      this.targetDate = 0;
     }
   }
-  
+
   disconnectedCallback() {
-    console.log("disconnected");
     clearInterval(this.timerId);
   }
-  toggleDrawer() {
-  }
+  toggleDrawer() {}
 }
 
-customElements.define('app-countdown', AppCountdown);
+customElements.define("app-countdown", AppCountdown);
